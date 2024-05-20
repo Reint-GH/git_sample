ae6b397d6e0883c704616cc58c18656190725460
Reint-GH <230030pu@yse-c.net>
2024-05-20 15:26:38 +0900

    first commit

---
+++ regist/complete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <main class="m-auto w-50">
+</head>
+<body>
+    <main class="m-auto w-50">
+        <h2 class="p-2 text-center">会員登録完了</h2>
+        <form action="add.php" method="post">
+        <p>
+            会員登録が完了しました。
+        </p>
+        <div class="d-flex mt-3">
+            <a href="../" class="btn-outline-primany w-50 me-2">トップページ</a>
+            <a href="../login/" class="btn-outline-primany w-50">ログイン</a>
+        </div>
+    </main>
+</body>
+</html>
